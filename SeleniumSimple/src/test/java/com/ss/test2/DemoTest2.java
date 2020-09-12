36ca4833b240a279eb5c624be10676b1956c6d41
jagru <jagru@LAPTOP-BG6T89RM>
2020-09-12 17:01:20 +0530

    Second Test Engineer Created

---
+++ SeleniumSimple/src/test/java/com/ss/test2/DemoTest2.java
@@ -0,0 +1,14 @@
+package com.ss.test2;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest2 
+{
+    @Test
+    public void test2_tc02()
+    {
+    	Reporter.log("TestEngineer2 created testscripts");
+    }
+    
+}
