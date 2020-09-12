409fe4a16bee404e8542b709bfcfb2868bb0ea34
jagru <jagru@LAPTOP-BG6T89RM>
2020-09-12 16:21:53 +0530

    Test1 Engineer created

---
+++ SeleniumSimple/src/test/java/com/ss/test/TestDemo.java
@@ -0,0 +1,14 @@
+package com.ss.test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestDemo 
+{
+	@Test
+	public void test_tc01()
+	{
+		Reporter.log("Test1 Engineer created this method");
+	}
+
+}
