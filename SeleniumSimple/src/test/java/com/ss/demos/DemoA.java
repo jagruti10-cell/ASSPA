d618e0a851568223da2a4898512f5e19cc387248
jagru <jagru@LAPTOP-BG6T89RM>
2020-09-12 15:53:11 +0530

    My first github Repository Framework

---
+++ SeleniumSimple/src/test/java/com/ss/demos/DemoA.java
@@ -0,0 +1,13 @@
+package com.ss.demos;
+
+import org.testng.Reporter;
+
+public class DemoA {
+
+	public static void main(String[] args) 
+	{
+		Reporter.log("Hello Welcome to GITHUB",true);
+		
+	}
+
+}
